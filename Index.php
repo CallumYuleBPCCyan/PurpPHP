7923759732beb25685a184eb7f863579ef827264
CallumYuleBPCCyan <32195155+CallumYuleBPCCyan@users.noreply.github.com>
2017-11-03 09:28:21 +0000

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,17 +1,9 @@
-
-<!Doctype html>
-<html>
-<body>
-
+<!Doctype html><html><body>
 <?php
-
 echo "First time trying PHP";
-
+$Roll = echo rand(1,6);
+if ($Roll = 1) {    echo "Almost snake Eyes";} 
+elseif ($Roll = 6) {    echo "Winner";} 
+else{    echo "Other number";}
 ?>
-
-</body>
-</html>
-
-
-
-
+</body></html>
