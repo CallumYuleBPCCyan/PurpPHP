fa661fdb5e25273eb0cb5e54b5e8b7430bce1a32
CallumYuleBPCCyan <32195155+CallumYuleBPCCyan@users.noreply.github.com>
2017-11-03 10:19:55 +0000

    Update Index.php

--- Index.php
+++ Index.php
@@ -2,8 +2,6 @@
     <body>        
         <?php        
         echo "First time trying PHP";        
-        $Roll = rand(1,6);        
-        echo $Roll;    
         ?>    
     </body>
 </html>
